1cfd53763e75a3db04b911ff79175fa76dfc21bf
Morgan Harold Bauer <mhb@cise.ufl.edu>
2009-10-16 20:05:08 -0400

    board reading functions

--- slitherlink.lisp
+++ slitherlink.lisp
@@ -36,6 +36,11 @@
 
 ;;; This reminds me of some of the rendering techniques and such used to determine whether we are inside or outside of a polygon.
 
+
+;;;; Constraints of solving the game
+;; Must be one single continuous loop (out-degree of each vertex is 2)
+;; Must not violate the numbers
+
 ;;;; Classes
 ;;;These class declarations are nowhere near complete and are just a basic structure to maybe hang ideas upon.
 
@@ -51,7 +56,7 @@
   (faces vertices))
 
 (defclass vertex ()
-  (faces edges))
+;;   (faces edges))
 
 ;; Maybe a board class to hold an array? But what else can it hold?
 ;; The array contains a structured edition of the game board
@@ -75,10 +80,6 @@
 
 
 
-
-
-
-
 (defconstant +offset+ 1)
 
 (defun print-board (board)
@@ -91,57 +92,77 @@
                            (format t "~A" (/ column 2)))
                           ((AND (zerop column) (evenp column))
                            (format t "~A" (/ row 2))))))
-       :do (format t "~&")))
-  )
+       :do (format t "~&"))))
 
 ;;; Read in a board from the user
-(defun read-game ()
-  (loop :for i = 0 :then (1+ i)
-     do (progn
-          (format t "Please insert ~:r line" (1+ i))
-          (split-sequence #\Space (read-line) :remove-empty-subseqs t))))
+(defun read-board-from-user ()
+  (let
+      ((dimensions (read-board-dimensions)))
+    (format t "Enter the lines with spaces in between the numbers~&")
+    (format t "and using 'n' for a blank space.~&")
+    (loop :for i :from 1 :to (cdr dimensions) ;; this is the y/height
+       collecting (read-board-row i))))
+
+(defun read-board-row (current-line-number)
+  (format t "~&Please insert ~:r line~&" current-line-number))
+
+(defun read-board-dimensions ()
+  (format t "What are the dimensions of the board?~&")
+  (format t "Enter as NxM, where N and M are numbers.~&")
+  (let
+      ((dimensions (mapcar #'parse-integer
+                           (split-sequence #\x (read-line) :remove-empty-subseqs t))))
+    (cons (car dimensions) (cadr dimensions))))
 
 ;;; Read in a board from a file
 (defun read-board-from-file (filename)
   (with-open-file (file-stream filename :direction :input)
     (let* ((board-size (split-sequence #\Space (read-line file-stream) :remove-empty-subseqs t)) ;we are reading the first line for the dimensions of the board.
            (height (parse-integer (cadr board-size))))
-      (loop for i from 1 to height
-         do (format t "Reading ~:r line...~&" i)
-         collecting (read-line file-stream)))))
+      (values (mapcar #'parse-integer board-size)
+              (loop for i from 1 to height
+                 do (format t "Reading ~:r line...~&" i)
+                 collecting (read-line file-stream))))))
+
+;; Tests for reading game form a file
 
 (test read-board-from-file
-  (is (equal (read-board-from-file "game1.txt")
-             '("3 3"
-               "0 0"))
-      (equal (read-board-from-file "game2.txt")
-             '("n n n 2 0"
-               "2 3 2 n 2"
-               "2 1 n 3 n"
-               "n 1 2 n 3"
-               "n 2 n n n"))))
+  (is (equal (MULTIPLE-VALUE-LIST (read-board-from-file "game1.txt"))
+             '((2 2)
+               ("3 3"
+                "0 0")))
+      (equal (MULTIPLE-VALUE-LIST (read-board-from-file "game2.txt"))
+             '((5 5)
+               ("n n n 2 0"
+                "2 3 2 n 2"
+                "2 1 n 3 n"
+                "n 1 2 n 3"
+                "n 2 n n n")))))
 
 
-;;; Parse the board read in from file or user.
+;; Parse the board read in from file or user.
 
-;; Count out edges
+
+(defun parse-board (rows columns))
+
+
+  ;; Count out edges
 ;;; use when traversing blindly (or not?)
-;; if out edges if out edges are  two (not including the in edge) or more OR more than 3 (including the in edge), then fail and backtrack. It is not clear which case would be better to optimize for
+  ;; if out edges if out edges are  two (not including the in edge) or more OR more than 3 (including the in edge), then fail and backtrack. It is not clear which case would be better to optimize for
 
-;; (out-breadth vertex) => a number n from 0 to 4, or 1 to 3, or something
+  ;; (out-breadth vertex) => a number n from 0 to 4, or 1 to 3, or something
 
-;; a function that coounts the crossings when moving ACROSS the board
-;; must have an even number of crossings
-;; application of even-odd rule
+  ;; a function that coounts the crossings when moving ACROSS the board
+  ;; must have an even number of crossings
+  ;; application of even-odd rule
 
 
 ;;; Theoretically I want test-game funciton that we give it a name, and a test games function where we gice it a list of games.
-(test game1
-  "Checks if game1 can be solved"
-  (is (= (solve "game1.txt") (parse-solution "game1solution.txt"))))
+  (test game1
+    "Checks if game1 can be solved"
+    (is (= (solve "game1.txt") (parse-solution "game1solution.txt"))))
 
-(test game2
-  "Checks if game2 can be solved"
-  (is (= (solve "game2.txt") (parse-solution "game2solution.txt"))))
-
+  (test game2
+    "Checks if game2 can be solved"
+    (is (= (solve "game2.txt") (parse-solution "game2solution.txt"))))
 
